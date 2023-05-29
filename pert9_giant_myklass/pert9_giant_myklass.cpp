e5779052a195321af25a54ee06e91a3887986bc6
giant-paw <giantpawbusiness@gmail.com>
2023-05-29 10:53:10 +0700

    pembuatan budi yang mempunyai object warisan dari pekerja dan pelajar

--- pert9_giant_myklass/pert9_giant_myklass.cpp
+++ pert9_giant_myklass/pert9_giant_myklass.cpp
@@ -29,6 +29,16 @@ public:
 	}
 };
 
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur) {
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
 int main()
 {
 }
